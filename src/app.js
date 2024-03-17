ac585886d3dd193efca5e9cadb36bcf6798433f8
Nakod55 <90890456+Nakod55@users.noreply.github.com>
2024-03-17 16:05:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.get("/api/v1/names/:id",(req,res)=>{
             status: "success", 
             message:"Product name fetched successfully",
             data:{
-                productName:elem
+                ProductName:elem
             }
         })
     }
